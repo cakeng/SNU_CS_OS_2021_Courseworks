dd7b028b6a5d3e3af56d3e3bcfd14c293fa87fc1
cakeng <cakeng@snu.ac.kr>
2021-10-03 03:20:01 +0900

    Tested prinfo fetching

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -1,7 +1,11 @@
 #include <linux/syscalls.h>
 #include <linux/prinfo.h>
 #include <linux/sched.h>
+#include <linux/cred.h>
+#include <linux/uidgid.h>
 #include <linux/list.h>
+
+#define __DEBUG_PTREE 1
 
 pid_t getChild(struct task_struct* task)
 {
@@ -19,16 +23,49 @@ pid_t getSibling(struct task_struct* task)
 {
 	return list_first_entry(&(task->sibling), struct task_struct, sibling)->pid;
 }
+void prinfoCopy(struct prinfo* out, struct task_struct* task)
+{
+	#ifdef __DEBUG_PTREE
+	printk("PTREE - prinfoCopy: Copying to location %lld", (uint64_t)out);
+	printk("PTREE - prinfoCopy: state:  %ld\n", task->state);
+	printk("PTREE - prinfoCopy: pid:  %d\n", task->pid);
+	printk("PTREE - prinfoCopy: parent_pid: %d\n", task->real_parent->pid);
+	printk("PTREE - prinfoCopy: first_child_pid:  %d\n", getChild(task));
+	printk("PTREE - prinfoCopy: next_sibling_pid:  %d\n", getSibling(task));
+	printk("PTREE - prinfoCopy: uid:  %lld\n", (int64_t)(task_uid(task).val));
+	printk("PTREE - prinfoCopy: comm:  %s\n", task->comm);
+	#endif
 
+	out->state = task->state;
+    out->pid = task->pid;
+    out->parent_pid = task->real_parent->pid;
+    out->first_child_pid = getChild(task);
+    out->next_sibling_pid = getSibling(task);
+    out->uid = (int64_t)(task_uid(task).val);
+    strncpy(out->comm, task->comm, TASK_COMM_LEN);
+}
 
 int ptree(struct prinfo *buf, int *nr)
 {
-	printk("Ptree Called!\n");
+	int i;
+	int bufferSize = *nr;
+	int processNum = 0;
+	int copiedNum = 0;
+	printk("PTREE - Ptree Called!\n");
 	read_lock(&tasklist_lock);
-	struct task_struct *pid0 = find_task_by_vpid(0);
-
+	for (i = 1; i < 10; i++)
+	{
+		#ifdef __DEBUG_PTREE
+			printk("PTREE - Processing pid %d\n", i);
+		#endif
+		prinfoCopy (buf + i, find_task_by_vpid(i));
+		processNum++;
+	}
+	
 	read_unlock(&tasklist_lock);
-	return 0;
+
+	*nr = processNum;
+	return copiedNum;
 }
 
 SYSCALL_DEFINE2(ptree, struct prinfo*, buf, int*, nr)
--- test/test_ptree.c
+++ test/test_ptree.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/syscall.h>
+#include <linux/types.h>
+#include "prinfo.h"
+
+#define BUFFER_SIZE 4096
 
 int main()
 {
+    int num = BUFFER_SIZE;
+    int* nr = &num;
+    int returnVal;
     printf ("Calling syscall 398...\n");
-	syscall(398, NULL, NULL);
+    struct prinfo* buf = malloc(sizeof(struct prinfo)*BUFFER_SIZE);
+	returnVal = syscall(398, buf, nr);
     printf ("Called syscall 398. Terminating.\n");
+
+    for (int i = 0; i < num; i++)
+    {
+        struct prinfo* prPtr = buf + i;
+        printf("Entry %d: %s,%d,%lld,%d,%d,%d,%lld\n", i, prPtr->comm, prPtr->pid, prPtr->state,
+  prPtr->parent_pid, prPtr->first_child_pid, prPtr->next_sibling_pid, prPtr->uid);
+    }
+
+    free(buf);
 	return 0;
 }
